6ecb32b1d6a75cd66bd02f43a120abba78e94a98
susanirago22 <susaniragoorani@gmail.com>
2022-12-02 01:16:26 +0300

    Grade.js

---
+++ Grade.js
@@ -0,0 +1,18 @@
+//UTILIY
+function printvalue(value){
+    console.info(value)
+}
+//TODO:switch case
+const grade=79
+switch(grade){
+    case 79:
+        console.log('grade','A')
+        case 60-79:
+            console.log('grade','B')
+            case 59-49:
+                console.log('grade','C')
+                case 40-49:
+                    console.log('grade','D')
+                    case 1-39:
+                        console.log('grade','E')
+}
