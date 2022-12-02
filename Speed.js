02bf288c94915989e2f9883e473621ad95725792
susanirago22 <susaniragoorani@gmail.com>
2022-12-02 09:46:23 +0300

    Speed.js

---
+++ Speed.js
@@ -0,0 +1,25 @@
+//UTILITY
+function printvalue(value){
+    console.info(value)
+}
+
+//TODO:redclarable variable
+    var speed=70
+    var speed="OK"
+    printvalue(speed)
+
+    var speed=75
+    var speed='point:1'
+    printvalue(speed)
+
+    var speed=80
+    var speed='points:2'
+    printvalue(speed)
+
+    var speed=135
+    var speed="License Suspended"
+    printvalue(speed)
+
+    
+    
+
